5eb45a49229c1649a482ec24fdd5784e59da5a64
JoseCurihuinca <jcurihuinca@outlook.com>
2025-11-19 15:23:05 -0300

    fix routes

--- src/actor/routes.js
+++ src/actor/routes.js
@@ -14,7 +14,7 @@ actorRoutes.post('/actor', handleInsertActorRequest);
 actorRoutes.get('/actores', handleGetActoresRequest);
 actorRoutes.get('/actor/:id', handleGetActorByIdRequest);
 actorRoutes.get('/actor/pelicula/:id', handleGetActoresByPeliculaIdRequest);
-actorRoutes.delete('/api/actor/:id', handleDeleteActorByIdRequest);
-actorRoutes.delete('/api/actores', handleDeleteAllActoresRequest);
+actorRoutes.delete('/actor/:id', handleDeleteActorByIdRequest);
+actorRoutes.delete('/actores', handleDeleteAllActoresRequest);
 
 export default actorRoutes;
